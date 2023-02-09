a75309620f27c8b64e89db9cb5befdcc11f44e23
Mohamed Ali <mohamedali08801@gmail.com>
2023-02-09 02:01:19 +0200

    added app icon and dependencies & created file structure

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'ui/pages/home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ToDo App',
+    return const MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ todo/lib/ui/pages/notification_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
