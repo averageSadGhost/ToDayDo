552bf6bb46d9fbef8652d4f328e34d3f1149f010
Mohamed Ali <mohamedali08801@gmail.com>
2023-02-11 04:01:06 +0200

    finished notification screen

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/route_manager.dart';
 import 'ui/pages/home_page.dart';
+import 'ui/pages/notification_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.teal,
+          // ignore: deprecated_member_use
+          backgroundColor: Colors.black),
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const NotificationScreen(payload: "Title | Description | 10:20"),
     );
   }
 }
--- todo/lib/ui/pages/notification_screen.dart
+++ todo/lib/ui/pages/notification_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/ui/theme.dart';
 
 class NotificationScreen extends StatefulWidget {
-  const NotificationScreen({Key? key}) : super(key: key);
+  const NotificationScreen({Key? key, required this.payload}) : super(key: key);
+  final String payload;
 
   @override
   // ignore: library_private_types_in_public_api
@@ -9,11 +12,165 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  String _payload = '';
+  @override
+  void initState() {
+    _payload = widget.payload;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          _payload.split('|')[0],
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: context.theme.primaryColor,
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                Text(
+                  "Hello, Mohamed",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                    color: Get.isDarkMode ? Colors.white : darkGreyClr,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "You have a new reminder",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                    color: Get.isDarkMode ? Colors.grey[100] : darkGreyClr,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: primaryClr,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.text_format,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            "Title",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.split('|')[0],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.description,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            "Description",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.split('|')[1],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.calendar_today_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                          SizedBox(width: 20),
+                          Text(
+                            "Date",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        _payload.split('|')[2],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
     );
   }
 }
