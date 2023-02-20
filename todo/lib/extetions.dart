4bc82ce25c77279116c2b26843752dca27eb92c1
Mohamed Ali <mohamedali08801@gmail.com>
2023-02-21 00:12:53 +0200

    Update extetions.dart

--- todo/lib/extetions.dart
+++ todo/lib/extetions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 extension EmptyPadding on num {
+  // This is a shortcut for sizedbox but it's not actually efficient since you will lose the const constructor I only used it for learning purposes.
   SizedBox get sbh => SizedBox(height: toDouble());
 
   SizedBox get sbw => SizedBox(width: toDouble());
